c80581acd5b0aa2dec17de665447846b87bb2b6d
Sukant Ghosh <sukantghosh@Sukants-MacBook-Air.local>
2024-11-05 17:44:16 +0000

    click handler added

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,24 +1,27 @@
 import { Fragment } from "react";
-
+import { MouseEvent } from "react";
 
 function ListGroup() {
+    let selectedIndex = 0;
     const items = [
         'NYC',
         'SF',
         'SD',
         'NO'
     ]
-
+    const handleClick = (event:MouseEvent) => console.log(event)
    return (
             <Fragment>
                 <h1> List </h1>
                 {items.length===0 && <p>No items found</p>}
                 <ul className="list-group">
-                {items.map((item) => 
-                    (<li className="list-group-item" 
+                {items.map((item, index) => 
+                    (<li className =  { selectedIndex === index ? 'list-group-item active' : 'list-group-item' }
                     key={item} 
-                    onClick ={() => console.log(item)}>
-                    {item}</li>))
+                    onClick = {handleClick}
+                    >
+                    {item}
+                    </li>))
                 }
                 </ul>
             </Fragment>
